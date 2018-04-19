95122e493b609c175bc4b8233236419ae3e338d4
Massimo Dalmazzi <massimodalmazzi@MacBook-Air-di-Massimo.local>
2018-04-19 10:56:19 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -19,7 +20,6 @@ app.post('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
-
 });
 
 app.get('/todos', (req, res) => {
@@ -27,8 +27,28 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  };
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+
+  }).catch((e) => {
+    res.status(400).send();
+    //Intenzionalmente non metto err possono esserci dati utente
   })
-})
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
