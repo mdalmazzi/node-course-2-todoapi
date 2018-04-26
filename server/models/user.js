70612f1cb3408c901600fb9e635f778adff6f434
Massimo Dalmazzi <massimodalmazzi@MacBook-Air-di-Massimo.local>
2018-04-26 15:27:34 +0200

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -71,8 +71,29 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.token': token,
     'tokens.access': 'auth'
   })
-
 };
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    // fatto perchè bcrypt gestisce solo callback e non promise
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user)
+        } else {
+          reject();
+        }
+      })
+    })
+  })
+};
+
 
 UserSchema.pre('save', function (next) {
   var user = this;
